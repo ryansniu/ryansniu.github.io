3794886c7075b2094a9c32f7f433614a5c0a57dc
TheBestestGuestest <ryanniu314@gmail.com>
2020-08-26 17:26:07 -0700

    testing out a nav bar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import useSticky from './hooks/useSticky.js'
+import Welcome from './components/Header/Welcome'
+import Navbar from './components/Header/Navbar'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { isSticky, element } = useSticky()
+    return (
+        <>
+            <Navbar sticky={isSticky} />
+            <Welcome element={element} />
+        </>
+    )
 }
 
-export default App;
+export default App
