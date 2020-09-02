8fe8319d66fc48017897b16ecc429430c6095918
TheBestestGuestest <ryanniu314@gmail.com>
2020-09-02 02:34:27 -0700

    progress on welcome section
    added links to github, linkedin, devpost, and email
    added some spicy animations
    probably will change an animation on mobile we'll see

--- src/sections/Welcome.js
+++ src/sections/Welcome.js
@@ -5,21 +5,36 @@ import './Welcome.css'
 const Welcome = () => {
     return (
             <main>
-                <Container fluid className='welcome'>
-                    <Row className='align-items-center'>
+                <Container fluid>
+                    <Row className='welcome'>
                         <Col lg={7}>
                             <Image className='pfp' src={process.env.PUBLIC_URL + '/pfp.jpg'} roundedCircle fluid alt='Ryan Niu'/>
                         </Col>
-                        <Col className='intro-text'>
-                            <h1 style={{fontSize: 60}}>Hi, I'm Ryan!</h1>
+                        <Col lg={5} className='intro-text'>
+                            <h1 style={{fontSize: 50}}>Hi, I'm Ryan!</h1>
                             <text>Computer Engineer from UC Santa Barbara
                                 trying to learn webdev but it clearly is a struggle.
                                 This website is also under construction.
                                 Try not to break anything!
                             </text>
+                            <div>
+                                <a href="https://github.com/ryansniu" target="_blank" rel="noopener noreferrer">
+                                    <Image className='home-icons' src={process.env.PUBLIC_URL + '/github-64.png'} fluid alt='Github'/>
+                                </a>
+                                <a href="https://www.linkedin.com/in/ryansniu/" target="_blank" rel="noopener noreferrer">
+                                    <Image className='home-icons' src={process.env.PUBLIC_URL + '/linkedin-64.png'} fluid alt='LinkedIn'/>
+                                </a>
+                                <a href="https://devpost.com/ryansniu" target="_blank" rel="noopener noreferrer">
+                                    <Image className='home-icons' src={process.env.PUBLIC_URL + '/devpost-64.png'} fluid alt='Devpost'/>
+                                </a>
+                                <a href="mailto:ryanniu314@gmail.com" target="_blank" rel="noopener noreferrer">
+                                    <Image className='home-icons' src={process.env.PUBLIC_URL + '/email-64.png'} fluid alt='Email'/>
+                                </a>
+                            </div>
                         </Col>
                     </Row>
                 </Container>
+
             </main>
     )
 }
